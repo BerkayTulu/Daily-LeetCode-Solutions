99d7053d322d443774d5d255a8a7c9396b278547
BerkayTulu <berkaytulu@gmail.com>
2024-02-28 16:11:20 +0300

    Adding solution

--- 513findBottomLeftValue.py
+++ 513findBottomLeftValue.py
@@ -8,6 +8,8 @@
 # Output: 7
 
 # Definition for a binary tree node.
+from collections import deque
+
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -15,7 +17,44 @@ class TreeNode(object):
         self.right = right
 class Solution(object):
     def findBottomLeftValue(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+        q = deque([root])
+        ans = 0
+        while q:
+            ans = q[0].val
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return ans
+
+    
+# Input: root = [1,2,3,4,5]
+# Output: 4
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+solution = Solution()
+bottom_left_value = solution.findBottomLeftValue(root)
+print(bottom_left_value) # 3
+
+# Input: root = [1,2,3,4,null,5,6,null,null,7]
+# Output: 7
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.right.left = TreeNode(5)
+root.right.right = TreeNode(6)
+root.right.left.left = TreeNode(7)
+
+solution = Solution()
+bottom_left_value = solution.findBottomLeftValue(root)
+
+print(bottom_left_value) # 7
