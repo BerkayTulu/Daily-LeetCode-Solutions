58c84062cb4f421f9bd1d02ac06e6c7d7d1f1967
BerkayTulu <berkaytulu@gmail.com>
2024-03-08 00:30:30 +0300

    add solution

--- 141hasCycle.py
+++ 141hasCycle.py
@@ -29,4 +29,32 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
+        # Floyd's Cycle Finding Algorithm
+
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+# Test the solution
+head = ListNode(3)
+head.next = ListNode(2)
+head.next.next = ListNode(0)
+head.next.next.next = ListNode(-4)
+head.next.next.next.next = head.next
+print(Solution().hasCycle(head)) # Expected: True
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = head
+print(Solution().hasCycle(head)) # Expected: True
+
+head = ListNode(1)
+print(Solution().hasCycle(head)) # Expected: False
+
+
         
