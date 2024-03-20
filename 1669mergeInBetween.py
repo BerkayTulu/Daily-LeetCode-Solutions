87d2aefbd37e4180245fb104eb52f5fa58fb27bb
BerkayTulu <berkaytulu@gmail.com>
2024-03-20 16:39:30 +0300

    add solution

--- 1669mergeInBetween.py
+++ 1669mergeInBetween.py
@@ -21,3 +21,37 @@ class ListNode(object):
 
 class Solution(object):
     def mergeInBetween(self, list1, a, b, list2):
+        dummy = ListNode(0, list1)
+        curr = dummy
+        for _ in range(a):
+            curr = curr.next
+        start = curr
+        for _ in range(b - a + 1):
+            curr = curr.next
+        end = curr.next
+        start.next = list2
+        while list2.next:
+            list2 = list2.next
+        list2.next = end
+        return dummy.next
+# Time: O(n)
+# Space: O(1)
+# Difficulty: medium
+def print_list(node):
+    while node:
+        print(node.val, end=" ")
+        node = node.next
+    print()
+
+# Test case
+list1 = ListNode(10, ListNode(1, ListNode(13, ListNode(6, ListNode(9, ListNode(5))))))
+a = 3
+b = 4
+list2 = ListNode(1000000, ListNode(1000001, ListNode(1000002)))
+sol = Solution()
+result = sol.mergeInBetween(list1, a, b, list2)
+
+print_list(result)
+# Output: 10 1 13 1000000 1000001 1000002 5
+    
+
