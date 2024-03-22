f4bacc299c588ce4f95587ffee1220a31ef1c99a
BerkayTulu <berkaytulu@gmail.com>
2024-03-22 23:32:40 +0300

    add new challenge

---
+++ 234isPalindrome.py
@@ -0,0 +1,23 @@
+# Given the head of a singly linked list, return true if it is a 
+# palindrome
+#  or false otherwise.
+
+# Example 1:
+# Input: head = [1,2,2,1]
+# Output: true
+
+# Example 2:
+# Input: head = [1,2]
+# Output: false
+ # Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        
