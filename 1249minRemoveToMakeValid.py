5a8b8b1ce63fb70820f0c081e46df0f1738f20e4
BerkayTulu <berkaytulu@gmail.com>
2024-04-06 17:39:12 +0300

    add solution

--- 1249minRemoveToMakeValid.py
+++ 1249minRemoveToMakeValid.py
@@ -19,6 +19,48 @@
 # Output: ""
 # Explanation: An empty string is also valid.
  
+# class Solution(object):
+#     def minRemoveToMakeValid(self, s):
+#         s = list(s)
+#         a = 0
+#         b = 0
+#         for i in range(len(s)):
+#             if s[i] == '(':
+#                 a += 1
+#             elif s[i] == ')':
+#                 if a > 0:
+#                     a -= 1
+#                 else:
+#                     s[i] = ''
+#         for i in range(len(s)-1, -1, -1):
+#             if s[i] == '(' and b > 0:
+#                 s[i] = ''
+#                 b -= 1
+#             elif s[i] == '(':
+#                 s[i] = ''
+#             elif s[i] == ')':
+#                 b += 1
+#         return ''.join(s)
 class Solution(object):
     def minRemoveToMakeValid(self, s):
-        
+        stack = []
+        to_remove = set()
+
+        for i, char in enumerate(s):
+            if char not in "()":
+                continue
+            if char == '(':
+                stack.append(i)
+            elif not stack:
+                to_remove.add(i)
+            else:
+                stack.pop()
+
+        to_remove = to_remove.union(set(stack))
+        return ''.join([char for i, char in enumerate(s) if i not in to_remove])
+#test case
+t = Solution()
+print(t.minRemoveToMakeValid("lee(t(c)o)de)")) #"lee(t(c)o)de"
+print(t.minRemoveToMakeValid("a)b(c)d")) #"ab(c)d"
+print(t.minRemoveToMakeValid("))((")) #""
+print(t.minRemoveToMakeValid("a)b(c)d")) #"ab(c)d"
