474af112652d50c1b5fff0764639395b65e3bbcd
BerkayTulu <berkaytulu@gmail.com>
2024-04-13 17:39:11 +0300

    I got help from AI for this challenge

--- 85maximalRectangle.py
+++ 85maximalRectangle.py
@@ -15,3 +15,52 @@
 
 class Solution(object):
     def maximalRectangle(self, matrix):
+        if not matrix:
+            return 0
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        left = [0] * n
+        right = [n] * n
+        height = [0] * n
+
+        max_area = 0
+
+        for i in range(m):
+            current_left, current_right = 0, n
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    height[j] += 1
+                else:
+                    height[j] = 0
+
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    left[j] = max(left[j], current_left)
+                else:
+                    left[j] = 0
+                    current_left = j + 1
+
+            for j in range(n - 1, -1, -1):
+                if matrix[i][j] == '1':
+                    right[j] = min(right[j], current_right)
+                else:
+                    right[j] = n
+                    current_right = j
+
+            for j in range(n):
+                max_area = max(max_area, (right[j] - left[j]) * height[j])
+
+        return max_area
+    
+# test case
+s = Solution()
+print(s.maximalRectangle([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])) # 6
+print(s.maximalRectangle([["0"]])) # 0
+print(s.maximalRectangle([["1"]])) # 1
+print(s.maximalRectangle([["0","0"]])) # 0
+print(s.maximalRectangle([["0","1"]])) # 1
+print(s.maximalRectangle([["1","0"]])) # 1
+print(s.maximalRectangle([["1","1"]])) # 2
+print(s.maximalRectangle([["1","1"],["1","1"]])) # 4
