ddbc4cdefb3c4045aebde0b0a605bb647bc75662
BerkayTulu <berkaytulu@gmail.com>
2024-05-14 01:41:42 +0300

    add solution

--- Unsolved/861matrixScore.py
+++ Unsolved/861matrixScore.py
@@ -14,3 +14,14 @@
 
 class Solution(object):
     def matrixScore(self, grid):
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            if grid[i][0] == 0:
+                for j in range(n):
+                    grid[i][j] ^= 1
+        for j in range(1, n):
+            count = sum(grid[i][j] for i in range(m))
+            if count < m - count:
+                for i in range(m):
+                    grid[i][j] ^= 1
+        return sum(int(''.join(map(str, row)), 2) for row in grid)
